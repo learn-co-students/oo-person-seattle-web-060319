7f011b0d324812c5eea932004fcf5668587e25e0
Karen Go <karenrgo@gmail.com>
2019-06-05 10:29:19 -0700

    test 3 passed

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,27 @@
 # your code goes here
+require 'pry'
+
+class Person
+
+	attr_reader :name
+	attr_accessor :bank_acct, :happiness
+
+	def initialize(name, bank_acct=25, happiness=8)
+		@name = name
+		@bank_acct = bank_acct
+		@happiness = happiness
+	end
+
+	def bank_account=(amt)
+		@bank_acct = amt
+	end
+
+	def happiness=(inx)
+		@happiness = inx
+		binding.pry
+	end
+
+
+end
+
+
