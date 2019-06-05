06a377f5d75f9cfd07c8dc3f05ab4e9298e9ca93
Karen Go <karenrgo@gmail.com>
2019-06-05 13:26:35 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -42,11 +42,67 @@ class Person
     else
       @hygiene = inx
     end
+    return @hygiene
   end
 
   def hygiene
     @hygiene
   end
+
+
+  def happy?
+  	if @happiness > 7
+  		true
+  	else
+  		false
+  	end
+  end
+
+   def clean?
+  	if @hygiene > 7
+  		true
+  	else
+  		false
+  	end
+  end
+
+  def get_paid(salary)
+  	@bank_acct += salary
+  	return "all about the benjamins"
+  end
+
+  def take_bath
+  	self.hygiene=(@hygiene+4)
+  	return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+  	self.hygiene=(@hygiene-3)
+  	self.happiness=(@happiness+2)
+  	return "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+  	self.happiness=(@happiness+3)
+  	friend.happiness=(friend.happiness+3)
+  	return "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+  	case topic
+  	when "politics"
+  		self.happiness=(@happiness-2)
+  		friend.happiness=(friend.happiness-2)
+  		return "blah blah partisan blah lobbyist"
+  	when "weather"
+  		self.happiness=(@happiness+1)
+  		friend.happiness=(friend.happiness+1)
+  		return "blah blah sun blah rain"
+  	else
+  		return "blah blah blah blah blah"
+  	end
+  end
+
 
 end
 
