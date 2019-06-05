2930a0cdde829d33f16b3283b9300c6031a24832
Dex Fitch <fitch.dexter@gmail.com>
2019-06-05 10:50:40 -0700

    tests passing up to 13

--- lib/person.rb
+++ lib/person.rb
@@ -3,24 +3,50 @@ require 'pry'
 
 class Person
 
-	attr_reader :name
-	attr_accessor :bank_acct, :happiness
+  attr_reader :name
 
-	def initialize(name, bank_acct=25, happiness=8)
-		@name = name
-		@bank_acct = bank_acct
-		@happiness = happiness
-	end
+  def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+    @name = name
+    @bank_acct = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+  end
 
-	def bank_account=(amt)
-		@bank_acct = amt
-	end
+  def bank_account=(amt)
+    @bank_acct = amt
+  end
 
-	def happiness=(inx)
-		@happiness = inx
-		binding.pry
-	end
+  def bank_account
+    @bank_acct
+  end
 
+  def happiness=(inx)
+    if inx > 10
+      @happiness = 10
+    elsif inx < 0
+      @happiness = 0
+    else
+      @happiness = inx
+    end
+  end
+
+  def happiness
+    @happiness
+  end
+
+  def hygiene=(inx)
+    if inx > 10
+      @hygiene = 10
+    elsif inx < 0
+      @hygiene = 0
+    else
+      @hygiene = inx
+    end
+  end
+
+  def hygiene
+    @hygiene
+  end
 
 end
 
